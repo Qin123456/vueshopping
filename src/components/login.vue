712bf0f9123b0890285ad961a0cf0a565b7f2320
行者！ <1006697795@qq.com>
2020-04-11 12:21:51 +0800

    完成登录

---
+++ src/components/login.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png">
+      </div>
+      <el-form class="login_form" ref="loginFormRef" :model="loginForm" :rules="loginRules" label-width="0px">
+        <el-form-item prop="username">
+          <el-input  prefix-icon="iconfont icon-yonghu" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input  prefix-icon="iconfont icon-lock" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item class="buts">
+          <el-button type="primary" @click="login('loginFormRef')">登录</el-button>
+          <el-button type="info" @click="resetForm('loginFormRef')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      loginRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
+    login (formName) {
+      this.$refs[formName].validate(async valid => {
+        if (!valid) {
+          this.$message.warning('验证未通过')
+        } else {
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status !== 200) {
+            this.$message.error('用户名或密码错误')
+          } else {
+            this.$message.success('登录成功')
+            window.sessionStorage.setItem('token', res.data.token)
+            this.$router.push('/home')
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.login_container{
+  background: #2b4b6b;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background: #fff;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  border-radius: 5px;
+}
+.avatar_box{
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 5px #eee;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  background-color: #fff;
+  img{
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.buts{
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../components/login.vue'
+import Home from '../components/home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // to and from are both route objects. must call `next`.
+  if (to.path === '/login') return next()
+  const token = window.sessionStorage.getItem('token')
+  if (!token) return next('/login')
+  next()
 })
 
 export default router
