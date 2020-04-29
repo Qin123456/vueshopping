c25ca6a26919acb1d89564ed25f28cd31251a7b6
行者！ <1006697795@qq.com>
2020-04-29 23:08:56 +0800

    完成项目

--- src/main.js
+++ src/main.js
@@ -10,12 +10,17 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 import axios from 'axios'
+import NProgress from 'nprogress' // Progress 进度条
+import 'nprogress/nprogress.css' // Progress 进度条样式
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
-
+axios.interceptors.response.use(config => {
+  NProgress.done()
+})
 Vue.prototype.$http = axios
 Vue.component('table-tree', tabletree)
 Vue.use(VueQuillEditor)
